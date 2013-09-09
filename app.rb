b9cf09511951f8b1180a29964ae7307da330a881
Burin Asavesna <burinasavesna@gmail.com>
2013-09-09 12:20:34 -0500

    making confluence box a textarea for easier copy/paste. redirecting root

--- app.rb
+++ app.rb
@@ -1,13 +1,16 @@
 require 'sinatra'
 require 'markdown2confluence'
 
+get '/' do
+   redirect('/gimme')
+end
 
 get '/gimme' do
-  "<form action='' enctype='multipart/form-data' method='POST'><textarea name='markdown' style='width: 600px; height: 500px; overflow: auto'>**Gimme some markdown**</textarea><input type='submit' /></form> "
+  "<h2>Markdown</h2><form action='' enctype='multipart/form-data' method='POST'><textarea name='markdown' style='width: 600px; height: 500px; overflow: auto'>**Gimme some markdown**</textarea><br><input type='submit' /></form> "
 end
 
 post '/gimme' do
   markdown = params[:markdown]
   confluence = Kramdown::Document.new(markdown).to_confluence
-  return "<h2></h2><form action='' enctype='multipart/form-data' method='POST'><textarea name='markdown' style='width: 600px; height: 500px; overflow: auto'>#{markdown}</textarea><input type='submit' /></form><h2>Confluence Markup</h2><pre>#{confluence}</pre>"
+  return "<h2>Markdown</h2><form action='' enctype='multipart/form-data' method='POST'><textarea name='markdown' style='width: 600px; height: 500px; overflow: auto'>#{markdown}</textarea><input type='submit' /></form><h2>Confluence Markup</h2><textarea name='markdown' style='width: 600px; height: 500px; overflow: auto'>#{confluence}</textarea>"
 end
